9f37e78c363bdd81b164f500e6b06b2c37a20ced
Karim <karim.mohraz@sap.com>
2017-01-05 21:17:56 +0100

    Initial Commit

---
+++ Cam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cam
+//
+//  Created by Mohraz, Karim on 1/5/17.
+//  Copyright © 2017 KM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
